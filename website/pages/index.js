bbfd964f9098f5238451c44ec7352f37fff562df
Elbert-Ainstein <iwu48@yahoo.com>
2022-12-01 14:26:02 +0800

    first commit

---
+++ website/pages/index.js
@@ -0,0 +1,20 @@
+import css from "../styles/Home.module.css";
+import { Canvas } from "@react-three/fiber";
+import Floor from "../components/Floor";
+
+export default function Home() {
+  return (
+    <div className={css.scene}>
+      <Canvas
+        shadows={true}
+        className={css.canvas}
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+          <Floor/>
+      </Canvas>
+    </div>
+  );
+}
+
